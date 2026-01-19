8b677528008c754d28ba5933c1b7049b0941dcd3
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-19 16:10:27 +0530

    Create verifyHospital.js

---
+++ backend/routes/hospital/verifyHospital.js
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+import express from 'express';
+import { authenticate } from '../../middleware/authenticate.js';
+import { Hospital } from '../../models/Hospital.js';
+
+const router = express.Router();
+
+// verify for valid hospital on every page
+router.get('/api/hospital/verify', authenticate, (req, res) = > {
+  return res.status(200).json({
+    success: true,
+    message: 'hospital verified'
+  });
+});
+
+// retrieves current logged in hospital data
+router.get('/api/user/get-data', authenticate, (req, res) = > {
+
+  const hospitalData = req.userData;
+
+  try{
+    const id = hospitalData.id;
+
+    const found = await Hospital.findById(id);
+
+    return res.status(200).json({
+      success: true,
+      message: 'hospital data fetched',
+      found
+    });
+  }
+  catch(err){
+     console.log("Error -> ", err);
+     return res.status(500).json({
+       success: false,
+       message: 'Something went wrong'
+     });
+   }
+});
+
+export default router;
