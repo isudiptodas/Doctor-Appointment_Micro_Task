8f57834f16444a76e708b5250cae9bb651783e82
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-19 19:42:31 +0530

    Create arcjetProtect.js

---
+++ backend/middleware/arcjetProtect.js
@@ -0,0 +1,29 @@
+import { arcjet } from '../config/arcjetSecurity.js';
+
+export const arcjetSecure = async (req, res, next) => {
+  
+  const decision = await arcjet.protect(req, { requested: 5 }); 
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({
+        success: false,
+        message: 'Too many request'
+       });
+     }
+      else if (decision.reason.isBot()) {
+        return res.status(403).json({
+        success: false,
+        message: 'Bot detected'
+      });
+     }
+      else {
+        return res.status(403).json({
+        success: false,
+        message: 'Forbidden'
+      });
+    }
+  }
+
+  return next();
+}
